f397b6b2544454c1d7e6d886554d87f381a5a360
Kulikov Alexander <shouter84@yandex.ru>
2023-09-23 13:33:39 +0300

    fixed some bugs

--- NoteBook.java
+++ NoteBook.java
@@ -2,15 +2,12 @@ import java.util.*;
 
 public class NoteBook {
 
-    private static String Brand;
-    private String Color;
-    private int RAM;
-    private int HDDvolume;
-    private int Diagonal;
-    private int Price;
-
-
-    private List<String> Store;
+    public String Brand;
+    public String Color;
+    public int RAM;
+    public int HDDvolume;
+    public int Diagonal;
+    public int Price;
 
     public NoteBook(String Brand, String Color, int RAM, int HDDvolume, int Diagonal, int Price) {
         this.Brand = Brand;
@@ -21,59 +18,11 @@ public class NoteBook {
         this.Price = Price;
     }
 
-
-    static String allBrands() {
-        return "Brand : " + Brand;
-    }
-
-//    public Map<Integer, String> numBrands() {
-//        int i = 1;
-//        while(!set.isEmpty) {
-//            hMapBrand.put(i, Brand);
-//            i++;
-//
-//        }
-//        return hMapBrand;
-//    }
-
-    public Map<Integer, String> hMapBrand = new HashMap<>();
-
-    public Set<NoteBook> set = new HashSet<>();
-    public Set<NoteBook> clientSet = new HashSet<>();
-
-
-//    public client(String Brand, String Color, int RAM, int HDDvolume, int Diagonal, int Price) {
-//
-//        NoteBook client = new NoteBook(Brand, Color, RAM, HDDvolume, Diagonal, Price);
-//
-//        Scanner cs = new Scanner(System.in);
-//
-//        System.out.println("Введите цифру отвечающую за Бренд:");
-//        int brandNum = cs.nextLine();
-//
-//        System.out.println("Введите цифру отвечающую за Цвет:");
-//        int colorNum = cs.nextLine();
-//
-//        System.out.println("Введите цифру отвечающую за Объем оперативной памяти:");
-//        int hddNum = cs.nextInt();
-//
-//        System.out.println("Введите цифру отвечающую за Диагональ экрана:");
-//        int diagonalNum = cs.nextInt();
-//
-//        System.out.println("Введите цифру отвечающую за Цену:");
-//        int priceNum = cs.nextInt();
-//
-//        clientSet.add(client);
-//        cs.close();
-//    }
-
-
     @Override
     public String toString() {
-        return "Brand : " + Brand + "Color : " + Color + "RAM : " + RAM + "HDDvolume : " + HDDvolume +
-                "Diagonal : " + Diagonal + "Price : " + Price + "\n";
+        return "Brand : " + Brand + "\t" + "Color : " + Color + "\t" + "RAM : " + RAM + "\t" + "HDDvolume : " + HDDvolume + "\t" +
+                "Diagonal : " + Diagonal + "\t" + "Price : " + Price + "\n";
     }
-
 
     @Override
     public boolean equals(Object obj) {
--- Task3.java
+++ Task3.java
@@ -4,48 +4,157 @@ public class Task3 {
     public static void main(String[] args) {
         intro();
         menu();
-//        System.out.println(set);
-//        System.out.println(cat2.equals(cat3));
-
-
-//
-//        Scanner cs = new Scanner(System.in);
-//        System.out.println("Введите число");
-//
-//        int number = cs.nextInt();
-
     }
+
     public static Set<NoteBook> set = new HashSet<>();
+
+    public static Set<String> setBrand = new HashSet<>();
+    public static Set<String> setColor = new HashSet<>();
+    public static Set<Integer> setRAM = new HashSet<Integer>();
+    public static Set<Integer> setHDDvolume = new HashSet<Integer>();
+    public static Set<Integer> setDiagonal = new HashSet<Integer>();
+    public static Set<Integer> setPrice = new HashSet<Integer>();
+
+    public static TreeMap<Integer, String> mapBrand = new TreeMap<>();
+    public static TreeMap<Integer, String> mapColor = new TreeMap<>();
+    public static TreeMap<Integer, Integer> mapRAM = new TreeMap<>();
+    public static TreeMap<Integer, Integer> mapHDDvolume = new TreeMap<>();
+    public static TreeMap<Integer, Integer> mapDiagonal = new TreeMap<>();
+    public static TreeMap<Integer, Integer> mapPrice = new TreeMap<>();
+
     private static void intro() {
-        NoteBook note1 = new NoteBook("Asus","Black", 4, 120, 15, 10000);
-        NoteBook note2 = new NoteBook("Asus","Black", 8, 120, 17, 12000);
-        NoteBook note3 = new NoteBook("Asus","White", 16, 120, 15, 20000);
-        NoteBook note4 = new NoteBook("Acer","Black", 4, 120, 15, 9000);
-        NoteBook note5 = new NoteBook("Acer","White", 8, 240, 17, 18000);
-        NoteBook note6 = new NoteBook("HP","Black", 8, 320, 19, 25000);
-        NoteBook note7 = new NoteBook("HP","Black", 4, 120, 15, 17000);
-        NoteBook note8 = new NoteBook("MacBook","White", 16, 500, 15, 85000);
-        NoteBook note9 = new NoteBook("MacBook","Black", 16, 1000, 17, 95000);
-        NoteBook note10 = new NoteBook("MacBook","Silver", 16, 2000, 19, 130000);
-        // System.out.println(cat1);
-        // System.out.println(cat2);
 
-        set.add(note1);
-        set.add(note2);
-        set.add(note3);
-        set.add(note4);
-        set.add(note5);
-        set.add(note6);
-        set.add(note7);
-        set.add(note8);
-        set.add(note9);
-        set.add(note10);
+        set.add(new NoteBook("Asus","Black", 4, 120, 15, 10000));
+        set.add(new NoteBook("Asus","Black", 8, 120, 17, 12000));
+        set.add(new NoteBook("Asus","White", 16, 120, 15, 20000));
+        set.add(new NoteBook("Asus","White", 16, 240, 15, 80000));
+        set.add(new NoteBook("Asus","White", 8, 480, 15, 120000));
+        set.add(new NoteBook("Asus","Black", 8, 120, 15, 10000));
+        set.add(new NoteBook("Asus","Black", 2, 240, 17, 12000));
+        set.add(new NoteBook("Asus","White", 2, 120, 15, 20000));
+        set.add(new NoteBook("Asus","White", 16, 240, 19, 80000));
+        set.add(new NoteBook("Asus","White", 4, 480, 15, 120000));
+        set.add(new NoteBook("Asus","Silver", 4, 120, 17, 10000));
+        set.add(new NoteBook("Asus","Silver", 2, 240, 17, 12000));
+        set.add(new NoteBook("Asus","Silver", 2, 120, 15, 20000));
+        set.add(new NoteBook("Asus","Silver", 16, 480, 17, 80000));
+        set.add(new NoteBook("Asus","Silver", 8, 480, 15, 120000));
 
-        System.out.println(set);
+        set.add(new NoteBook("HP","Black", 8, 240, 19, 20000));
+        set.add(new NoteBook("HP","White", 4, 120, 15, 12000));
+        set.add(new NoteBook("HP","Silver", 6, 240, 19, 20000));
+        set.add(new NoteBook("HP","Silver", 2, 120, 15, 12000));
+        set.add(new NoteBook("HP","White", 16, 480, 15, 120000));
+        set.add(new NoteBook("HP","Silver", 2, 480, 19, 120000));
+        set.add(new NoteBook("HP","Black", 8, 480, 17, 80000));
+        set.add(new NoteBook("HP","Black", 4, 320, 15, 20000));
+        set.add(new NoteBook("HP","Silver", 16, 480, 19, 120000));
+        set.add(new NoteBook("HP","White", 16, 240, 17, 130000));
+        set.add(new NoteBook("HP","Silver", 6, 480, 19, 37000));
+        set.add(new NoteBook("HP","White", 4, 320, 15, 16000));
+        set.add(new NoteBook("HP","Silver", 16, 480, 19, 120000));
+        set.add(new NoteBook("HP","Black", 6, 320, 19, 20000));
+        set.add(new NoteBook("HP","White", 2, 120, 15, 16000));
+        set.add(new NoteBook("HP","Silver", 16, 120, 17, 120000));
+        set.add(new NoteBook("HP","Black", 6, 480, 17, 20000));
+        set.add(new NoteBook("HP","Black", 4, 320, 15, 16000));
+
+        set.add(new NoteBook("Acer","Silver", 4, 250, 19, 16000));
+        set.add(new NoteBook("Acer","White", 16, 480, 17, 80000));
+        set.add(new NoteBook("Acer","Black", 16, 480, 15, 80000));
+        set.add(new NoteBook("Acer","Silver", 4, 240, 19, 16000));
+        set.add(new NoteBook("Acer","White", 2, 480, 17, 80000));
+        set.add(new NoteBook("Acer","Silver", 16, 120, 17, 10000));
+        set.add(new NoteBook("Acer","White", 8, 340, 19, 16000));
+        set.add(new NoteBook("Acer","Black", 16, 320, 17, 80000));
+        set.add(new NoteBook("Acer","Silver", 16, 320, 15, 20000));
+        set.add(new NoteBook("Acer","Black", 4, 120, 15, 10000));
+        set.add(new NoteBook("Acer","White", 8, 240, 17, 18000));
+        set.add(new NoteBook("Acer","Silver", 16, 120, 17, 10000));
+        set.add(new NoteBook("Acer","White", 8, 340, 19, 16000));
+        set.add(new NoteBook("Acer","Black", 16, 480, 15, 90000));
+        set.add(new NoteBook("Acer","White", 4, 240, 19, 16000));
+
     }
-
-
-    static void menu() {
+    public static void allBrands() {
+        for (NoteBook item: set) {
+            setBrand.add(item.Brand);
+        }
+        int i = 1;
+        for (String item : setBrand) {
+            mapBrand.put(i, item); // заполнение коллекции mapBrand
+            i++;
+        }
+        for (Map.Entry entry: mapBrand.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue() + "\n");
+        }
+    }
+    public static void allColors() {
+        for (NoteBook item: set) {
+            setColor.add(item.Color);
+        }
+        int i = 1;
+        for (String item : setColor) {
+            mapColor.put(i, item); // заполнение коллекции mapColor
+            i++;
+        }
+        for (Map.Entry entry: mapColor.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue() + "\n");
+        }
+    }
+    public static void allRAM() {
+        for (NoteBook item: set) {
+            setRAM.add(item.RAM);
+        }
+        int i = 1;
+        for (int item : setRAM) {
+            mapRAM.put(i, item); // заполнение коллекции mapRAM
+            i++;
+        }
+        for (Map.Entry entry: mapRAM.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue() + "\n");
+        }
+    }
+    public static void allHDDvolume() {
+        for (NoteBook item: set) {
+            setHDDvolume.add(item.HDDvolume);
+        }
+        int i = 1;
+        for (int item : setHDDvolume) {
+            mapHDDvolume.put(i, item); // заполнение коллекции mapHDDvolume
+            i++;
+        }
+        for (Map.Entry entry: mapHDDvolume.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue() + "\n");
+        }
+    }
+    public static void allDiagonal() {
+        for (NoteBook item: set) {
+            setDiagonal.add(item.Diagonal);
+        }
+        int i = 1;
+        for (int item : setDiagonal) {
+            mapDiagonal.put(i, item); // заполнение коллекции mapDiagonal
+            i++;
+        }
+        for (Map.Entry entry: mapDiagonal.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue() + "\n");
+        }
+    }
+    public static void allPrices() {
+        for (NoteBook item: set) {
+            setPrice.add(item.Price);
+        }
+        int i = 1;
+        for (int item : setPrice) {
+            mapPrice.put(i, item); // заполнение коллекции mapPrice
+            i++;
+        }
+        for (Map.Entry entry: mapPrice.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue() + "\n");
+        }
+    }
+    public static void menu() {
 
         Scanner cs = new Scanner(System.in);
         System.out.println("Добро пожаловать в наш магазин техники\n" +
@@ -60,21 +169,73 @@ public class Task3 {
             case 1:
                 System.out.println(set);
                 break;
-//            case 2:
-//                client();
-//                break;
+            case 2:
+                compare();
+                break;
             case 3:
-                NoteBook.allBrands();
                 break;
 
             default:
                 System.out.println("Ошибка! Введите цифру среди предложенных в меню!");
                 break;
         }
-
         cs.close();
 
     }
+    public static void compare() {
+        NoteBook client = client();
+        for (NoteBook item : set) {
+            if(item.equals(client)) {
+                System.out.println(item);
+            }
+        }
+    }
 
+    public static NoteBook client() {
+
+        String Brand;
+        String Color;
+        int RAM;
+        int HDDvolume;
+        int Diagonal;
+        int Price;
+
+        Scanner cs = new Scanner(System.in);
+
+        System.out.println("Введите цифру отвечающую за Бренд:");
+        Task3.allBrands();
+        int brandNum = cs.nextInt();
+        Brand = mapBrand.get(brandNum);
+
+        System.out.println("Введите цифру отвечающую за Цвет:");
+        Task3.allColors();
+        int colorNum = cs.nextInt();
+        Color = mapColor.get(colorNum);
+
+        System.out.println("Введите цифру отвечающую за Объем оперативной памяти:");
+        Task3.allRAM();
+        int ramNum = cs.nextInt();
+        RAM = mapRAM.get(ramNum);
+
+        System.out.println("Введите цифру отвечающую за Объем жесткого диска:");
+        Task3.allHDDvolume();
+        int hddNum = cs.nextInt();
+        HDDvolume = mapHDDvolume.get(hddNum);
+
+        System.out.println("Введите цифру отвечающую за Диагональ экрана:");
+        Task3.allDiagonal();
+        int diagonalNum = cs.nextInt();
+        Diagonal = mapDiagonal.get(diagonalNum);
+
+        System.out.println("Введите цифру отвечающую за Цену:");
+        Task3.allPrices();
+        int priceNum = cs.nextInt();
+        Price = mapPrice.get(priceNum);
+        cs.close();
+
+        NoteBook client = new NoteBook(Brand, Color, RAM, HDDvolume, Diagonal, Price);
+
+        return client;
+    }
 
 }
